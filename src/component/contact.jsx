aafa1a1185ecbbabc3b5c0392777a2ffa9cf4c0f
Angel727524 <cheonsa676@gmail.com>
2026-06-02 11:44:43 +0900

    first commit

---
+++ src/component/contact.jsx
@@ -0,0 +1,11 @@
+function Contact(){
+    return(
+        <>
+        <h2>Contact Information</h2>
+        <p>Tel :010-222-222</p>
+        <p>Email:you@gmail.com</p>
+
+        </>
+    )
+}
+export default Contact;
